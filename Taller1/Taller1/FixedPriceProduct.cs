015b9141979e5d1c2482bf4e2d7775254ce6f44e
EDGAR ANDRES MARTINEZ ZAPATA <edgarmartinez229064@correo.itm.edu.co>
2022-02-21 01:03:33 -0500

    Taller 1 - Version 1

---
+++ Taller1/Taller1/FixedPriceProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller1
+{
+    public class FixedPriceProduct : Product
+    {
+        public override decimal GetValueToPay()
+        {
+            return Price * (1 + (Decimal)Tax);
+        }
+
+        public override string ToString()
+        {
+            return $"{ base.ToString()}" +
+                $"\n{$" ",7}Price......: {$"{Price:C2}",14}" +
+                $"\n{$" ",7}Tax........: {$"{Tax:P2}",15}" +
+                $"\n{$" ",7}Value......: {$"{GetValueToPay():C2}",14}";
+        }
+    }
+}
---
+++ Taller1/Taller1/Invoice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller1
+{
+    internal class Invoice
+    {
+        private List<Product> _products = new();
+
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+        
+        public decimal GetValueToPay()
+        {
+            decimal Sum = 0;
+            _products.ForEach(
+                p => Sum += p.GetValueToPay()
+            );
+
+            return Sum;
+        }
+
+        public override string ToString()
+        {
+            return $"RECEIPT" +
+                $"\n-------------------------------------------------" +
+                $"\n{string.Join("\n", _products)}" +
+                $"\n{$" ",18} ===============" +
+                $"\n{$"TOTAL: ", -19} {$"{GetValueToPay():C2}",14}"; 
+        }
+    }
+}
